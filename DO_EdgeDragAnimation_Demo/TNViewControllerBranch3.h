878f9f0c61e76a6e5001d940712d8157216145ef
LWTFLH <13841302445@163.com>
2016-10-18 16:33:41 +0800

    add  b3

---
+++ DO_EdgeDragAnimation_Demo/TNViewControllerBranch3.h
@@ -0,0 +1,13 @@
+//
+//  TNViewControllerBranch3.h
+//  LW_EdgeDragAnimation_Demo
+//
+//  Created by 李伟 on 16/10/18.
+//  Copyright © 2016年 TNWLKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNViewControllerBranch3 : UIViewController
+
+@end
---
+++ DO_EdgeDragAnimation_Demo/TNViewControllerBranch3.m
@@ -0,0 +1,38 @@
+//
+//  TNViewControllerBranch3.m
+//  LW_EdgeDragAnimation_Demo
+//
+//  Created by 李伟 on 16/10/18.
+//  Copyright © 2016年 TNWLKJ. All rights reserved.
+//
+
+#import "TNViewControllerBranch3.h"
+
+@interface TNViewControllerBranch3 ()
+
+@end
+
+@implementation TNViewControllerBranch3
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"add 3");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
